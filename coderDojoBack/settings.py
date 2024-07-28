1f119856b8b76caaab59396f3b2e78019a1c3ddd
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-28 05:37:35 -0500

    Configurando Postgresql con psycopg2 para produccion

--- coderDojoBack/settings.py
+++ coderDojoBack/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,11 +132,9 @@ WSGI_APPLICATION = 'coderDojoBack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+print(os.getenv('DATABASE_URL'))
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
